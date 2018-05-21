f3c0217418f7e8dd98292cab16a2b43ffc527474
nvaneck <natvaneck@gmail.com>
2018-05-21 13:25:47 -0400

    testchange

--- blink/stream/index.js
+++ blink/stream/index.js
@@ -3,7 +3,7 @@
 // const STREAM_SERVER_ADDR = "https://localhost:4000";
 const HTTPS_PORT = 443;
 const MAIN_SERVER_ADDR = "http://blink.blinkcdn.com:8080";
-const STREAM_SERVER_ADDR = "https://stream.blinkcdn.com";
+const STREAM_SERVER_ADDR = "https://.blinkcdn.com";
 
 const express = require('express');
 const https = require('https');
