200900597c9e814cd994966ac990b5a30df94f5f
nvaneck <natvaneck@gmail.com>
2018-06-12 13:20:16 -0400

    Password Implementation 3.4.6
    Minor bug fixes

--- blink/stream/index.js
+++ blink/stream/index.js
@@ -381,7 +381,7 @@ function onSubscribe(socket, roomName) {
 }
 
 function onQuery(socket, roomName) {
-    if(streamRooms[roomName] !=== null) {
+    if(streamRooms[roomName] !== undefined) {
         console.log("A room exists with the pin " + streamRooms[roomName].pin);
         socket.emit('query response', true, streamRooms[roomName].pin);
     }
