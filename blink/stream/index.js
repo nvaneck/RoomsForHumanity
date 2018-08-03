33f81573bbd4b060cd6a905a18cbb5b4fc70805a
nvaneck <natvaneck@gmail.com>
2018-08-03 14:15:26 -0400

    Disconnect Bugfixes

--- blink/stream/index.js
+++ blink/stream/index.js
@@ -155,7 +155,7 @@ function onDisconnect(userID, roomName) {
                 sockets[clientID].emit('disconnect user', userID, roomName);
             }
 
-            if(length(streamRooms[roomName].clients) == 0) {
+            if(streamRooms[roomName].clients.length == 0) {
                 delete streamRooms[roomName];
             }
         }
